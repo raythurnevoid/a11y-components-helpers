b8a0121c9b789a84fc9fb90554a2684dfb47d140
Ray Thurne Void <ray.thurne.void@gmail.com>
2023-05-20 20:54:45 +0100

    breadcrumb

---
+++ src/routes/breadcrumb/+page.svelte
@@ -0,0 +1,12 @@
+<ol>
+	<li>
+		<a href="/home"> Home </a>
+	</li>
+	<li>
+		<a href="/home/sub"> Sub </a>
+	</li>
+	<li>
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<a aria-current="page"> Current </a>
+	</li>
+</ol>
